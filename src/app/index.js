83a29415400881317c158555b269262d61ad8c8a
elvingonzalezb <ing.elvingonzalez@gmail.com>
2020-07-12 03:55:12 -0500

    update

--- src/app/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Render, render } from 'react-dom';
-import App from './App';
-import './index.css';
-
-render(<App/>, document.getElementById('app'));
--- src/app/key.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    DES_KEY_API: 'f37aac6bcc821f8ed3f6e19a13739103'
-}
